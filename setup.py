dbbe923d5bb97109db7379bdfd8b755d63724ab6
Vishnunarayan K I <appukuttancr@gmail.com>
2018-05-27 20:42:33 +0530

    1.1 release

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='anime-downloader',
-    version='1.0',
+    version='1.1',
     author='Vishnunarayan K.I.',
     author_email='vishnunarayan6105@gmail.com',
     description='Download your favourite anime',
     packages=find_packages(),
     url='https://github.com/vn-ki/anime-downloader',
-    download_url='https://github.com/vn-ki/anime-downloader/archive/1.0.tar.gz',
+    download_url='https://github.com/vn-ki/anime-downloader/archive/1.1.tar.gz',
     keywords=['anime', 'downloader'],
     install_requires=[
         'bs4',
