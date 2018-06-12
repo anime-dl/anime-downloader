660a4e3a06b7a3d875513fcfd6281699b2c38958
Vishnunarayan K I <appukuttancr@gmail.com>
2018-06-12 11:17:11 +0530

    Make tests more sensible

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,20 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--anime-url',
+        '--anime-site',
         action='store',
-        default='https://www4.9anime.is/watch/kochinpa.p6l6/j6ooy2',
-        help='URL for (kochinpa!) anime for a specific site'
+        default='nineanime',
+        help='A specific anime site'
     )
 
 
 @pytest.fixture
 def anime_url(request):
-    return request.config.getoption('--anime-url')
+    site = request.config.getoption('--anime-site')
+
+    if site == 'nineanime':
+        return 'https://www4.9anime.is/watch/kochinpa.p6l6/j6ooy2'
+    elif site == 'kissanime':
+        return 'http://kissanime.ru/Anime/Kochin-Pa'
+    elif site == 'gogoanime':
+        return 'https://www2.gogoanime.se/category/kochin-pa-'
