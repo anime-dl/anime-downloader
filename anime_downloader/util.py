e69dcaea02b62da47051737bb540bc9ba0453d1b
Kash <39453935+KashTag@users.noreply.github.com>
2018-06-19 13:14:29 +0530

    Update util.py

--- anime_downloader/util.py
+++ anime_downloader/util.py
@@ -46,10 +46,10 @@ def search(query):
     except Exception as e:
         logging.error(click.style(str(e), fg='red'))
         sys.exit(1)
-    click.echo(format_search_results(search_results))
     except ValueError: 
         logging.error('No such Anime found. Please ensure correct spelling.')
         sys.exit(1)
+    click.echo(format_search_results(search_results))
     val = click.prompt('Enter the anime no: ', type=int, default=1)
 
     try:
