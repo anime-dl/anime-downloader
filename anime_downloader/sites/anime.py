7b9a3589ae5492d5fe0a4422278aebb45b72e75e
Vishnunarayan K I <appukuttancr@gmail.com>
2018-06-20 01:10:44 +0530

    Make gogoanime default for now

--- anime_downloader/sites/anime.py
+++ anime_downloader/sites/anime.py
@@ -114,17 +114,16 @@ class BaseEpisode:
         try:
             self.getData()
         except NotFoundError:
-            pass
-            # for quality in parent.QUALITIES:
-            #     parent.QUALITIES.remove(self.quality)
-            #     logging.warning('Quality {} not found. Trying {}.'.format(self.quality, quality))
-            #     self.quality = quality
-            #     try:
-            #         self.getData()
-            #         parent.quality = self.quality
-            #         break
-            #     except NotFoundError:
-            #         pass
+            for quality in parent.QUALITIES:
+                parent.QUALITIES.remove(self.quality)
+                logging.warning('Quality {} not found. Trying {}.'.format(self.quality, quality))
+                self.quality = quality
+                try:
+                    self.getData()
+                    parent.quality = self.quality
+                    break
+                except NotFoundError:
+                    pass
 
     def getData(self):
         raise NotImplementedError
