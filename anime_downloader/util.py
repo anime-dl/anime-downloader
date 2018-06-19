219aed2702b318bdb2d7cc79d8780632a0c09cb8
Kash <39453935+KashTag@users.noreply.github.com>
2018-06-19 17:10:19 +0530

    Update util.py

--- anime_downloader/util.py
+++ anime_downloader/util.py
@@ -42,13 +42,14 @@ def search(query):
     # cli. But it is used in watch too. :(
     try:
         search_results = NineAnime.search(query)
-        if not search_results:
-            logging.error('No such Anime found. Please ensure correct spelling.')
-            sys.exit(1)
     except Exception as e:
         logging.error(click.style(str(e), fg='red'))
         sys.exit(1)
     click.echo(format_search_results(search_results))
+    
+    if not search_results:
+        logging.error('No such Anime found. Please ensure correct spelling.')
+        sys.exit(1)
     
     val = click.prompt('Enter the anime no: ', type=int, default=1)
 
@@ -57,7 +58,7 @@ def search(query):
         title = search_results[val-1].title
     except IndexError:
         logging.error('Only maximum of {} search results are allowed.'
-                      ' Please input a number less than {}'.format(len(search_results),len(search_results)+1)
+                      ' Please input a number less than {}'.format(len(search_results),len(search_results)+1))
         sys.exit(1)
 
     logging.info('Selected {}'.format(title))
