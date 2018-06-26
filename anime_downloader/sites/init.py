77b83ca46ff6cf6f7c3c2859ba22a76d735309ae
Vishnunarayan K I <appukuttancr@gmail.com>
2018-06-26 16:46:02 +0530

    Handle cfscrape not found

--- anime_downloader/sites/init.py
+++ anime_downloader/sites/init.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+import logging
 
 ALL_ANIME_SITES = [
     # ('filename', 'sitename', 'classname')
@@ -12,5 +13,16 @@ ALL_ANIME_SITES = [
 def get_anime_class(url):
     for site in ALL_ANIME_SITES:
         if site[1] in url:
-            module = import_module('anime_downloader.sites.{}'.format(site[0]))
+            try:
+                module = import_module(
+                    'anime_downloader.sites.{}'.format(site[0])
+                )
+            except ImportError as e:
+                # TODO: This should raise an error instead of logging.
+                # I'm lazy af right now.
+                logging.debug("Coudn't import {}, '{}'".format(site[0], e.msg))
+                logging.warning("Provider '{}' not used. Make sure you have "
+                                "cfscrape and node-js installed".format(site[0]))
+                continue
+
             return getattr(module, site[2])
