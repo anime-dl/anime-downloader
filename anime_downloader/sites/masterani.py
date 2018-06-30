db043b0e4d6a7f1b82632632188af676cb9a65d0
Vishnunarayan K I <appukuttancr@gmail.com>
2018-07-01 01:06:24 +0530

    Masterani!!

--- anime_downloader/sites/masterani.py
+++ anime_downloader/sites/masterani.py
@@ -1,7 +1,9 @@
-from anime_downloader.sites.anime import BaseAnime, BaseEpisode
 import json
+import re
 import cfscrape
-import requests
+
+from anime_downloader.sites.anime import BaseAnime, BaseEpisode
+from anime_downloader.const import desktop_headers
 
 scraper = cfscrape.create_scraper()
 
@@ -10,7 +12,25 @@ class MasteraniEpisode(BaseEpisode):
     QUALITIES = ['360p', '480p', '720p', '1080p']
 
     def _get_sources(self):
-        res = scraper.get(self.url)
+        res = scraper.get(self.url, headers=desktop_headers)
+        sources_re = re.compile(r'mirrors:.*?(\[.*?\])')
+        quality = int(self.quality[:-1])
+
+        sources = json.loads(sources_re.findall(res.text)[0])
+
+        ret = []
+        for source in sources:
+            if source['quality'] != quality:
+                continue
+
+            url = source['host']['embed_prefix'] + source['embed_id']
+
+            if source['host']['embed_suffix']:
+                url += source['host']['embed_suffix']
+
+            ret.append((source['host']['name'], url))
+
+        return ret
 
 
 class Masterani(BaseAnime):
@@ -19,18 +39,17 @@ class Masterani(BaseAnime):
     _api_url = 'https://www.masterani.me/api/anime/{}/detailed'
     _episodeClass = MasteraniEpisode
 
-    @classmethod
     def get_data(self):
         anime_id = self.url.split('info/')[-1].split('-')[0]
         url = self._api_url.format(anime_id)
-        res = scraper.get(url).json()
+        res = scraper.get(url, headers=desktop_headers).json()
         base_url = 'https://www.masterani.me/anime/watch/{}'.format(
             res['info']['slug']) + '/'
 
         episode_urls = []
         for episode in res['episodes']:
             url = base_url + episode['info']['episode']
-            episode_urls.append(url)
+            episode_urls.append((episode['info']['episode'], url))
 
         self._episode_urls = episode_urls
         self.meta = res['info']
