748ff1b3cc8909c92aadc49feae6e24a563663b7
ngomile <ngomile932@gmail.com>
2018-08-19 22:51:23 +0200

    mp4upload not yet implemented reason

--- anime_downloader/extractors/mp4upload.py
+++ anime_downloader/extractors/mp4upload.py
@@ -2,5 +2,7 @@ from anime_downloader.extractors.base_extractor import BaseExtractor
 
 
 class MP4Upload(BaseExtractor):
-    '''
+    '''Having difficulty implement an extractor for mp4upload
+    so for now it is left empty until I or someone else figures
+    it out
     '''
