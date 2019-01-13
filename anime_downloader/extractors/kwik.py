5b569868e3679c6596c5c90de8ce870fac593050
Ayush Mittal <29ayush@gmail.com>
2019-01-13 22:46:41 +0530

    Updated Comment

--- anime_downloader/extractors/kwik.py
+++ anime_downloader/extractors/kwik.py
@@ -9,10 +9,10 @@ session = session.get_session()
 
 
 class Kwik(BaseExtractor):
-    '''Extracts video url from mp4upload embed pages, performs a request
-    back to the non-embed mp4upload page to extract the title of the video
-    albeit imperfectly as mp4upload doesn't place full title on the main
-    page of whichever video you are dealing with.
+    '''Extracts video url from kwik pages, Kwik has some `security` 
+       which allows to access kwik pages when only refered by something
+       and the kwik video stream when refered through the corresponding 
+       kwik video page.
     '''
     def _get_data(self):
         
@@ -25,9 +25,9 @@ class Kwik(BaseExtractor):
         
         #Kwik servers don't have direct link access you need to be referred
         #from somewhere, I will just use the url itself.
-        mp4u_embed = session.get(self.url, headers={'referer': self.url}).text
+        kwik_text = session.get(self.url, headers={'referer': self.url}).text
         
-        stream_url_pattern, source_parts_list = source_parts_re.search(mp4u_embed).group(1,2)
+        stream_url_pattern, source_parts_list = source_parts_re.search(kwik_text).group(1,2)
         source_parts_list = source_parts_list.split('|')
         stream_url = ""
         print('fkme')
