f297e30ab47a76af19845e043d91bbf59e558534
ngomile <ngomile932@gmail.com>
2019-04-13 11:54:26 +0200

    Revert changes to match master

--- anime_downloader/extractors/mp4upload.py
+++ anime_downloader/extractors/mp4upload.py
@@ -18,35 +18,30 @@ class MP4Upload(BaseExtractor):
         # Extract the important bits from the embed page, with thanks to the
         # code I saw from github user py7hon in his/her mp4upload-direct
         # program as inspiration for this. Only with regex.
-        try:
-            source_parts_re = re.compile(
-                                    r'.*?100\|(.*?)\|.*?\|video\|(.*?)\|(\d+)\|.*?',
-                                    re.DOTALL)
+        source_parts_re = re.compile(
+                                r'.*?100\|(.*?)\|.*?\|video\|(.*?)\|(\d+)\|.*?',
+                                re.DOTALL)
 
-            mp4u_embed = session.get(self.url).text
-            domain, video_id, protocol = source_parts_re.match(mp4u_embed).groups()
+        mp4u_embed = session.get(self.url).text
+        domain, video_id, protocol = source_parts_re.match(mp4u_embed).groups()
 
-            logging.debug('Domain: %s, Video ID: %s, Protocol: %s' %
-                          (domain, video_id, protocol))
+        logging.debug('Domain: %s, Video ID: %s, Protocol: %s' %
+                      (domain, video_id, protocol))
 
-            url = self.url.replace('embed-', '')
-            # Return to non-embed page to collect title
-            mp4u_page = BeautifulSoup(session.get(url).text, 'html.parser')
+        url = self.url.replace('embed-', '')
+        # Return to non-embed page to collect title
+        mp4u_page = BeautifulSoup(session.get(url).text, 'html.parser')
 
-            title = mp4u_page.find('span', {'class': 'dfilename'}).text
-            title = title[:title.rfind('_')][:title.rfind('.')].replace(' ', '_')
+        title = mp4u_page.find('span', {'class': 'dfilename'}).text
+        title = title[:title.rfind('_')][:title.rfind('.')].replace(' ', '_')
 
-            logging.debug('Title is %s' % title)
+        logging.debug('Title is %s' % title)
 
-            # Create the stream url
-            stream_url = 'https://{}.mp4upload.com:{}/d/{}/{}.mp4'
-            stream_url = stream_url.format(domain, protocol, video_id, title)
+        # Create the stream url
+        stream_url = 'https://{}.mp4upload.com:{}/d/{}/{}.mp4'
+        stream_url = stream_url.format(domain, protocol, video_id, title)
 
-            logging.debug('Stream URL: %s' % stream_url)
-        except AttributeError:
-            logging.debug('Failed to extract stream URL')
-            stream_url = ''
-            title = ''
+        logging.debug('Stream URL: %s' % stream_url)
 
         return {
             'stream_url': stream_url,
