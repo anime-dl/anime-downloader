1a74230f71a96baa1dbc9f68aaa22fab125eb7bc
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-03-10 13:55:23 +0100

    fixed

--- anime_downloader/extractors/kwik.py
+++ anime_downloader/extractors/kwik.py
@@ -3,8 +3,8 @@ import jsbeautifier
 import re
 from anime_downloader.extractors.base_extractor import BaseExtractor
 from anime_downloader.sites import helpers
-logger = logging.getLogger(__name__)
 
+logger = logging.getLogger(__name__)
 
 class Kwik(BaseExtractor):
     '''Extracts video url from kwik pages, Kwik has some `security`
@@ -16,7 +16,7 @@ class Kwik(BaseExtractor):
     def _get_data(self):
 
         # Kwik servers don't have direct link access you need to be referred
-        # from somewhere, I will just use the url itself
+        # from somewhere, I will just use the url itself.
 
         download_url = self.url.replace('kwik.cx/e/', 'kwik.cx/f/')
         kwik_text = helpers.get(download_url, referer=download_url).text
