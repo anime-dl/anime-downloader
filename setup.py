a896d71c6eedd9e4599ad7730861970748a5a25c
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-03-10 13:32:51 +0100

    added jsbeautifier

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'requests-cache>=0.4.13',
         'tabulate>=0.8.3',
         'pycryptodome>=3.8.2',
+        'jsbeautifier>=1.10.3',
     ],
     extras_require={
         'dev': [
