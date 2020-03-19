604ee7ca487824695562ac6cf424c0338f0fdd24
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-03-19 23:03:11 +0000

    added A2zanime to init

--- anime_downloader/sites/init.py
+++ anime_downloader/sites/init.py
@@ -13,6 +13,7 @@ ALL_ANIME_SITES = [
     ('animefreak', 'animefreak', 'AnimeFreak'),
     ('animeflix', 'animeflix', 'AnimeFlix'),
     ('dubbedanime','dubbedanime','Dubbedanime'),
+    ('a2zanime','a2zanime','A2zanime'),
 ]
 
 
