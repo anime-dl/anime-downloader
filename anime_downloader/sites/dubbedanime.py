57767cd3d0e395b83a8607bcb778512a1565eed9
Vishnunarayan K I <appukuttancr@gmail.com>
2020-03-20 23:02:30 +0530

    chore: add to readme and style

--- anime_downloader/sites/dubbedanime.py
+++ anime_downloader/sites/dubbedanime.py
@@ -20,7 +20,7 @@ class Dubbedanime(Anime, sitename='dubbedanime'):
             }).json()
 
             title_data = {
-                'data' : [] 
+                'data' : []
             }
             for a in range(len(search_results['results'])):
                 url = cls.url + search_results['results'][a]['url']
@@ -43,8 +43,8 @@ class Dubbedanime(Anime, sitename='dubbedanime'):
             soup = helpers.soupify(helpers.get(self.url))
             elements = soup.find("ul", {"id": "episodes-grid"}).select('li > div > a')
 
-            episode_links = []  
-            for a in elements[::-1]: 
+            episode_links = []
+            for a in elements[::-1]:
                 episode_links.append('https://dubbedanime.net' + a.get('href'))
 
             return [a for a in episode_links]
@@ -54,14 +54,15 @@ class Dubbedanime(Anime, sitename='dubbedanime'):
             self.title= soup.select('h1.h3')[0].text
 
 class DubbedanimeEpisode(AnimeEpisode, sitename='dubbedanime'):
-        def _get_sources(self): 
+        def _get_sources(self):
+            # TODO: refractor to extractor
             soup = helpers.soupify(helpers.get(self.url)).text
             xuath = r"xuath = '[^']*"
             x = re.search(xuath,soup).group().split("xuath = '")[1]
             tokenregex = r'"trollvid","id":"[^"]*'
             token = re.search(tokenregex,soup).group(0).split('"trollvid","id":"')[1]
             url = f'https://mp4.sh/embed/{token}{x}'
-            
+
             soup = helpers.get(url, referer=self.url).text
             url = re.search(r'source src="[^"]*',soup).group().split('source src="')[1]
 
