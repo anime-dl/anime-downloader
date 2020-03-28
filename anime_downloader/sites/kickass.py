6a833c0d282fa41592d8e58004493527a74c0cf6
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-03-28 13:31:46 +0100

    Add files via upload

--- anime_downloader/sites/kickass.py
+++ anime_downloader/sites/kickass.py
@@ -5,9 +5,11 @@ import json
 from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
 from anime_downloader.sites import helpers
 
-class KickAss(Anime, sitename='kickassanime'):
-        sitename = 'kickassanime'
-        url = f'https://{sitename}.rs/search'
+logger = logging.getLogger(__name__)
+
+class KickAss(Anime, sitename='kickass'):
+        sitename = 'kickass'
+        url = f'https://kickassanime.rs/search'
 
         @classmethod
         def search(cls, query):
@@ -37,7 +39,7 @@ class KickAss(Anime, sitename='kickassanime'):
             
             regex = r'\[{[\W\w]*?}]'
             episodes = json.loads(re.search(regex,str(soup)).group())
-         
+
             return [f'https://kickassanime.rs{a["slug"]}' for a in episodes[::-1]]
 
         def _scrape_metadata(self):
@@ -47,25 +49,52 @@ class KickAss(Anime, sitename='kickassanime'):
             info = json.loads(re.search(regex,str(soup)).group()+']}')
             self.title = info['name']
             
-class KickAssEpisode(AnimeEpisode, sitename='kickassanime'):
+class KickAssEpisode(AnimeEpisode, sitename='kickass'):
         def _get_sources(self):
+            server = self.config['server']
+            fallback = self.config['fallback_servers']
+            ext_fallback = self.config['ext_fallback_servers']
+
             soup = helpers.soupify(helpers.get(self.url))
             regex = r'{"clip[\w\W]*?}\]} '
             elements = json.loads(re.search(regex,str(soup)).group())
             links = ['link1','link2','link3','link4']
             sources_list = [] #Primary sources which links to more sources
+            ext_servers = []
             for a in links:
                 if len((elements['episode'][a]).replace(' ','')) != 0:
                     sources_list.append(elements['episode'][a])
-
+            if elements['ext_servers']:
+                for a in elements['ext_servers']:
+                    ext_servers.append(a)
             soup = helpers.get(sources_list[0]).text
             regex = r'\[{[\W\w]*?}\]'
             sources = re.search(regex,str(soup))
-            
-            if not sources: #Either vidstream or haloani
-                regex = r"[^/]window\.location = '[^']*"
-                sources = re.search(regex,str(soup)).group()[20:]
-                return [('vidstream', sources,)]
+
+            if not sources:
+                regex = r"[^/]window\.location = '([^']*)"
+                sources = re.search(regex,str(soup))
+                if sources:
+                    return [('vidstream', sources.group(1),)]
+                else:
+                    if len(ext_servers) == 0:
+                        return ''
+                    for i in range(2):
+                        for a in ext_servers:
+                            if a in ext_fallback or i == 1:
+                                if a['name'] == 'Vidstreaming' or a['name'] == 'Vidcdn':
+                                    return [('vidstream', a['link'],)]
+                                else:
+                                    return [('haloani', a['link'],)]
 
             sources = json.loads(sources.group())
-            return [('haloani', sources[0]['src'],)]
+            for i in range(3):
+                if i == 1:logger.debug('Preferred server "%s" not found. Trying all supported servers.',self.config['server'])
+                if i == 2:logger.warning('No supported servers found. Trying all servers. This will most likely not work')
+                for a in sources:
+                    if i == 1:
+                        for b in fallback:
+                            if a['name'] == b:
+                                return [('haloani', a['src'],)]
+                    if a['name'] == self.config['server'] or i == 2:
+                        return [('haloani', a['src'],)]
