fe21b5802037c5632395168ab2eb57fd4dcb6dd9
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-03-28 19:33:35 +0100

    fixed sites
    fixed the bad code

--- anime_downloader/sites/dubbedanime.py
+++ anime_downloader/sites/dubbedanime.py
@@ -7,7 +7,6 @@ from anime_downloader.sites import helpers
 class Dubbedanime(Anime, sitename='dubbedanime'):
         sitename = 'dubbedanime'
         url = f'https://{sitename}.net'
-
         @classmethod
         def search(cls, query):
             search_results = helpers.post(f'https://ww5.dubbedanime.net/ajax/paginate',
@@ -18,36 +17,18 @@ class Dubbedanime(Anime, sitename='dubbedanime'):
                 'size': 30,
                 'letter': 'all',
             }).json()
-
-            title_data = {
-                'data' : []
-            }
-            for a in range(len(search_results['results'])):
-                url = cls.url + search_results['results'][a]['url']
-                title = search_results['results'][a]['title']
-                data = {
-                    'url' : url,
-                    'title' : title,
-                }
-                title_data['data'].append(data)
-
             search_results = [
                 SearchResult(
-                    title=result["title"],
-                    url=result["url"])
-                for result in title_data.get('data', [])
+                    title=search_results['results'][a]['title'],
+                    url=cls.url + search_results['results'][a]['url'])
+                for a in range(len(search_results['results']))
             ]
             return(search_results)
 
         def _scrape_episodes(self):
             soup = helpers.soupify(helpers.get(self.url))
             elements = soup.find("ul", {"id": "episodes-grid"}).select('li > div > a')
-
-            episode_links = []
-            for a in elements[::-1]:
-                episode_links.append('https://dubbedanime.net' + a.get('href'))
-
-            return [a for a in episode_links]
+            return [('https://dubbedanime.net' + a.get('href')) for a in elements[::-1]]
 
         def _scrape_metadata(self):
             soup = helpers.soupify(helpers.get(self.url))
@@ -57,9 +38,9 @@ class DubbedanimeEpisode(AnimeEpisode, sitename='dubbedanime'):
         def _get_sources(self):
             soup = helpers.soupify(helpers.get(self.url)).text
 
-            x = re.search(r"xuath = '[^']*", soup).group().replace("xuath = '",'')
-            token = re.search(r'"trollvid","id":"[^"]*', soup).group(0).replace('"trollvid","id":"','')
-            
+            x = re.search(r"xuath = '([^']*)", soup).group(1)
+            token = re.search(r'"trollvid","id":"([^"]*)', soup).group(1)
+
             url = f'https://mp4.sh/embed/{token}{x}'
             return [('mp4sh', url)]
 
