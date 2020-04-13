63b9759108f17236a9f5376341c4f2a6c7e872db
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-04-13 14:57:38 +0000

    added quality selection to vidstream

--- anime_downloader/extractors/vidstream.py
+++ anime_downloader/extractors/vidstream.py
@@ -1,6 +1,5 @@
 import logging
 import re
-import sys
 from anime_downloader.extractors.base_extractor import BaseExtractor
 from anime_downloader.sites import helpers
 
@@ -8,15 +7,28 @@ logger = logging.getLogger(__name__)
 
 class VidStream(BaseExtractor):
     def _get_data(self):
+        QUALITIES = {
+        "360":[],
+        "480":[],
+        "720":[],
+        "1080":[],
+        }
+
         url = self.url.replace('https:////','https://')
         soup = helpers.get(url).text
         regex = r'https://vidstreaming\.io/download\?[^"]*'
         download = re.search(regex,soup).group()
             
         soup = helpers.soupify(helpers.get(download))
-        link = soup.select('div.dowload > a')[0].get('href')
+        links = soup.select('div.mirror_link')[0].select('div.dowload > a')
         
+        for a in QUALITIES:
+            for b in links:
+                if a in b.text:
+                    QUALITIES[a].append(b.get('href'))
+
+        stream_url = QUALITIES[self.quality[:-1]][0] if QUALITIES != {"360":[],"480":[],"720":[],"1080":[],} else links[0].get('href') #In case nothing is found
         return {
-            'stream_url': link,
+            'stream_url': stream_url,
             'referer': download
         }
