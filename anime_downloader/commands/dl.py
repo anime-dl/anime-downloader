90a1f0ec0a67f721a490da3dcc0e3fbe74c57a78
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-04-26 15:06:20 +0000

    Update dl.py

--- anime_downloader/commands/dl.py
+++ anime_downloader/commands/dl.py
@@ -79,6 +79,8 @@ def command(ctx, anime_url, episode_range, url, player, skip_download, quality,
             external_downloader, chunk_size, disable_ssl, fallback_qualities, choice, skip_fillers):
     """ Download the anime using the url or search for it.
     """
+    query = anime_url[:]
+
     util.print_info(__version__)
     # TODO: Replace by factory
     cls = get_anime_class(anime_url)
@@ -105,13 +107,11 @@ def command(ctx, anime_url, episode_range, url, player, skip_download, quality,
 
     if download_dir and not skip_download:
         logger.info('Downloading to {}'.format(os.path.abspath(download_dir)))
-
     if skip_fillers:
-        fillers = util.get_filler_episodes()
-
+        fillers = util.get_filler_episodes(query)
     for episode in animes:
         if skip_fillers and fillers:
-            if str(episode.ep_no) in fillers:
+            if episode.ep_no in fillers:
                 logger.info("Skipping episode {} because it is a filler.".format(episode.ep_no))
                 continue
         
