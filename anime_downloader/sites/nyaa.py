d746868692fc6e1b9c1a05935bfdabc4b02ae2dd
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-04-30 13:13:41 +0000

    added nyaa.si site
    huge thanks to arjix and iggy!

---
+++ anime_downloader/sites/nyaa.py
@@ -0,0 +1,28 @@
+import re
+
+from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
+from anime_downloader.sites import helpers
+
+class Nyaa(Anime, sitename = 'nyaa'):
+    sitename = 'nyaa'
+    url = f'https://{sitename}.si'
+    @classmethod
+    def search(cls,query):
+        rex = r'(magnet:)+[^"]*'
+        search_results = helpers.soupify(helpers.get(f"https://nyaa.si/?q={query}&f=0&c=3_1&s=seeders&o=desc"))
+        
+        search_results = [
+            SearchResult(
+                title = i.select("a:not(.comments)")[1].get("title"),
+                url = i.find_all('a',{'href':re.compile(rex)})[0].get('href'))
+            for i in search_results.select("tr.default,tr.success")
+            ]
+
+        return search_results
+        
+    def _scrape_episodes(self):
+        return [self.url] #the magnet has all episodes making this redundant
+
+class NyaaEpisode(AnimeEpisode, sitename='nyaa'):
+    def _get_sources(self):
+        return [('no_extractor', self.url,)]
