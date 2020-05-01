ec7640d7229228ab42428f46e5c079fcbf6535ad
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-05-01 11:12:56 +0000

    made vidstream more flexible and fixed error

--- anime_downloader/extractors/vidstream.py
+++ anime_downloader/extractors/vidstream.py
@@ -2,7 +2,7 @@ import logging
 import re
 from anime_downloader.extractors.base_extractor import BaseExtractor
 from anime_downloader.sites import helpers
-
+import sys
 logger = logging.getLogger(__name__)
 
 class VidStream(BaseExtractor):
@@ -13,12 +13,15 @@ class VidStream(BaseExtractor):
         "720":[],
         "1080":[],
         }
-
         url = self.url.replace('https:////','https://')
         soup = helpers.get(url).text
         regex = r'https://vidstreaming\.io/download\?[^"]*'
-        download = re.search(regex,soup).group()
-            
+        
+        if re.search(regex,self.url): #both the download link and stream link can be passed to this
+            download = self.url
+        else:
+            download = re.search(regex,soup).group()
+
         soup = helpers.soupify(helpers.get(download))
         links = soup.select('div.mirror_link')[0].select('div.dowload > a')
         
@@ -27,7 +30,12 @@ class VidStream(BaseExtractor):
                 if a in b.text:
                     QUALITIES[a].append(b.get('href'))
 
-        stream_url = QUALITIES[self.quality[:-1]][0] if QUALITIES != {"360":[],"480":[],"720":[],"1080":[],} else links[0].get('href') #In case nothing is found
+        stream_url = QUALITIES[self.quality[:-1]][0] if len(QUALITIES[self.quality[:-1]]) != 0 else ''
+        
+        if QUALITIES == {"360":[],"480":[],"720":[],"1080":[],}:
+            stream_url = links[0].get('href') #In case nothing is found
+            loggger.debug("The streaming link's quality cannot be identified.")
+
         return {
             'stream_url': stream_url,
             'referer': download
