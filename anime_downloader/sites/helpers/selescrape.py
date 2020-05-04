8a17c7204ef97a8f28229f0e6104fbea3bb8c991
ArjixGamer <53124886+ArjixGamer@users.noreply.github.com>
2020-05-04 20:01:09 +0300

    made the changes 0x requested and also improved some parts of the code.

--- anime_downloader/sites/helpers/selescrape.py
+++ anime_downloader/sites/helpers/selescrape.py
@@ -2,88 +2,88 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from anime_downloader.const import get_random_header
 from selenium.webdriver.common.by import By
+import requests, os, click, time, json
 from urllib.parse import urlencode
 from urllib.parse import urlsplit
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from logging import exception
 from sys import platform
-import requests, os
-import click
-import time
-import json
 
-header = get_random_header()
 def test_os():
-    if platform.startswith("linux"):
+    if platform.startswith("lin"):
         os_name = "linux"
-    elif platform == "darwin":
+    elif platform.startswith("da"):
         os_name = "darwin"
-    elif platform == "win32":
+    elif platform.startswith("win"):
         os_name = "win32"
     return os_name
-gg = test_os()
-if gg.startswith("linux"):
-    dd = '\\'
-    aa = '/'
-elif gg() == 'win32':
-    dd = '/'
-    aa = '\\'
-else:
-    raise exception('This OS is not currently supported.')
+
 def get_config(): #can't use config because of circular import
     APP_NAME = 'anime downloader'
-    directory = str(click.get_app_dir(APP_NAME) + aa + 'data').replace(dd, aa)
+    directory = os.path.join(click.get_app_dir(APP_NAME), 'data') #click.get_app_dir(APP_NAME) + slash_2_use + 'data'
     return directory
 
-def browsers():
-    config = get_config()
+
+def get_browser():
+    ostest = test_os()
     common_bs = ['chrome', 'firefox']
-    if gg == "linux":
+    if ostest == "linux":
         common_bs = common_bs[0]
-    elif gg == "darwin":
-        print('Mac OS is not supported yet.')
-    elif gg == "win32":
+    elif ostest == "darwin":
+        # 'Safari browser is not supported.'
+        common_bs = common_bs[0]
+    elif ostest == "win32":
         common_bs = common_bs[0]
     return common_bs
 
-def add_url_params(url, params):
-    a = urlencode(params)
-    b = url + '?' + a
-    return b
 
-default_browser = browsers()
-def driver_select(name=default_browser):
+def add_url_params(url, params):
+    encoded_params = urlencode(params)
+    url = url + '?' + encoded_params
+    return url
+
+
+def driver_select():
+    browser = get_browser()
     data_dir = get_config()
-    if name == 'firefox' or name == 'ff' or name == 'f':
+    if browser == 'firefox':
         fireFoxOptions = webdriver.FirefoxOptions()
         fireFoxOptions.headless = True
         fireFoxOptions.add_argument('--log fatal')
         fireFoxOptions.add_argument('-CreateProfile f"Selenium_firefox {data_dir}"')
+        adblock = os.path.join(data_dir, 'Extensions', 'adblock.xpi')
+        profile_path = os.path.join(data_dir, 'Selenium_firefox')
         try:
-            fireFoxOptions.add_argument(f'--install-app {data_dir}{aa}Extensions{aa}adblock.xpi')
+            fireFoxOptions.add_argument(f'--install-app {adblock}')
         except:
             pass
         try:
             driver = webdriver.Firefox(firefox_options=fireFoxOptions)
         except:
-            driver = webdriver.Firefox(firefox_options=fireFoxOptions, firefox_profile=f'{data_dir}{aa}Selenium_firefox', service_log_path=os.path.devnull)
-    elif name == 'chrome' or name == 'chromium' or name == 'c':
+            driver = webdriver.Firefox(firefox_options=fireFoxOptions, firefox_profile=f'{profile_path}', service_log_path=os.path.devnull)
+
+
+    elif browser == 'chrome':
         from selenium.webdriver.chrome.options import Options
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         try:
-            chrome_options.add_argument(f"load-extension={data_dir}{aa}Extensions{aa}ublock")
+            extension_path = os.join.path(data_dir, 'Extensions', 'ublock.crx')
+            chrome_options.add_extension(extension_path)
         except:
             pass
-        chrome_options.add_argument(f'--log-path {data_dir}{aa}chromedriver.log')
-        chrome_options.add_argument(f"--user-data-dir={data_dir}{aa}Selenium_chromium")
+        header = get_random_header()
+        profile_path = os.path.join(data_dir, 'Selenium_chromium')
+        log_path = os.path.join(data_dir, 'chromedriver.log')
+        chrome_options.add_argument(f'--log-path {log_path}')
+        chrome_options.add_argument(f"--user-data-dir={profile_path}")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--window-size=1920,1080")
         user_agent = header
         chrome_options.add_argument(f'user-agent={user_agent}')
-        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
     return driver
 
 
@@ -106,7 +106,7 @@ def status_select(driver, url, status='hide'):
 def cloudflare_wait(driver):
     try:
         title = driver.title  # title = "Just a moment..."
-        while title == "Just a moment...":
+        while title == "Just a moment...": #or "another title for different kinds of Cloudflare protection."
             title = driver.title
             if not title == "Just a moment...":
                 break
@@ -121,7 +121,7 @@ def request(request_type, url, headers={}, **kwargs): #Headers not yet supported
             params = value
 
     new_url = add_url_params(url, params)
-    driver = driver_select(default_browser)
+    driver = driver_select()
     status = status_select(driver, new_url, 'hide')
 
     try:
@@ -134,8 +134,9 @@ def request(request_type, url, headers={}, **kwargs): #Headers not yet supported
         driver.close()
 
 def get_by_css_selector(url, params, css, attr):
+    browser = get_browser()
     new_url = add_url_params(url, params)
-    driver = driver_select(default_browser)
+    driver = driver_select(browser)
     status = status_select(driver, new_url, 'hide')
     try:
         cloudflare_wait(driver)
@@ -144,9 +145,10 @@ def get_by_css_selector(url, params, css, attr):
         driver.save_screenshot("screenshot.png");
         driver.close()
 
-def get_by_xpath(url, params, full_xpath, attr, browser=default_browser):
+def get_by_xpath(url, params, full_xpath, attr):
+    browser = get_browser()
     new_url = add_url_params(url, params)
-    driver = driver_select(default_browser)
+    driver = driver_select(browser)
     status = status_select(driver, new_url, 'hide')
     try:
         cloudflare_wait(driver)
