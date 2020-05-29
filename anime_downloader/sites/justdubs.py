c4ce9df2fa5eaca36a438d68c14f529a44f11da1
nate-moo <40650681+nate-moo@users.noreply.github.com>
2020-05-29 18:39:22 -0400

    Update justdubs.py

--- anime_downloader/sites/justdubs.py
+++ anime_downloader/sites/justdubs.py
@@ -39,25 +39,60 @@ class JustDubs(Anime, sitename='justdubs'):
 class JustDubsEpisode(AnimeEpisode, sitename='justdubs'):
     def _get_sources(self):
         server = self.config['server']
-        #server_links = {
-        #    'mp4upload':'https://mp4upload.com/embed-{}.html',
-        #    'gcloud':'https://gcloud.live/v/{}',
-        #    'fembed':'https://www.fembed.com/v/{}',
-        #}
+        server_links = {
+            'mp4upload':'https://mp4upload.com/embed-{}.html',
+            'gcloud':'https://gcloud.live/v/{}',
+            'fembed':'https://www.fembed.com/v/{}',
+        }
         soup = helpers.soupify(helpers.get(self.url))
-        link = soup.find('iframe').get("src")
-        #hosts = json.loads(soup.select(f"div.tab-pane iframe[src*='{server_links['mp4upload' or 'gcloud' or 'fembed']}']".format(server_links)))
-        #logger.debug(hosts)
-        #_type = hosts[0]["type"]
-        
-
-        #try:
-        #    host = list(filter(lambda video: video["host"] == server and video["type" == _type, hosts]))[0]
-        #except IndexError:
-        #    host = hosts[0]
-        #    if host["host"] == "mp4upload" and len(hosts) > 1:
-        #        host = hosts[1]
-        #name = host["host"]
-        #_id = host["id"]
-        #link = self.getLink(name, _id)
-        return [('mp4upload', link)]
+        link = soup.find_all('iframe')
+        regexmp4 = '^https://mp4upload.com/\>'
+        regexgc = '^https://gcloud.live/\>'
+        regexfe = '^https://www.fembed.com/\>'
+        returnServer = ""
+        returnLink = ""
+        for x in link:
+            y = x.get("src")
+            logger.debug(y)
+            if re.search(regexmp4, y) == True:
+                returnServer = 'mp4upload'
+                returnLink = y
+                logger.debug("MP4Upload")
+                return [('mp4upload', y)]
+            elif re.search(regexgc, y) == True:
+                returnServer = 'gcloud'
+                returnLink = y
+                logger.debug("gcloud")
+                return [('gcloud', y)]
+            elif re.search(regexfe, y) == True:
+                returnServer = 'fembed'
+                returnLink = y
+                logger.debug("Fembed")
+                return [('fembed', y)]
+#
+#
+#        logger.debug(link)
+#        return [(returnServer, y)]
+#        hosts = json.loads(soup.select(f"div.tab-pane iframe[src*='{server_links['mp4upload' or 'gcloud' or 'fembed']}']".format(server_links)))
+#        logger.debug(hosts)
+#        _type = hosts[0]["type"]
+#
+#        for i in link:
+#            link[i].get("src")
+#            if server_links["mp4upload"] in link[i]:
+#                return [('mp4upload', link[i])]
+#            elif server_links["gcloud"] in link[i]:
+#                return [('gcloud', link[i])]
+#            elif server_links['fembed'] in link[i]:
+#                return [('fembed'), link[1]]
+#
+#        try:
+#            host = list(filter(lambda video: video["host"] == server and video["type" == _type, hosts]))[0]
+#        except IndexError:
+#            host = hosts[0]
+#            if host["host"] == "mp4upload" and len(hosts) > 1:
+#                host = hosts[1]
+#        name = host["host"]
+#        _id = host["id"]
+#        link = self.getLink(name, _id)
+#        return [('mp4upload', link)]
