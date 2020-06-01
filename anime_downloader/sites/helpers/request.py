b9c37442188f45fca20159dfa077af763fb79742
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-01 16:05:12 +0300

    changed it from an Exception to a warning and used cf_scrape if selenium fails to work.

--- anime_downloader/sites/helpers/request.py
+++ anime_downloader/sites/helpers/request.py
@@ -58,7 +58,8 @@ def setup(func):
                 from anime_downloader.sites.helpers import selescrape
                 sess = selescrape
             except ImportError:
-                raise Exception("You can't use this provider because it requires selenium to work.\nIf you want to install it then run:  'pip install selenium' .")
+                sess = cf_session
+                logger.warning("You can't use this provider because it requires selenium to work.\nIf you want to install it then run:  'pip install selenium' .")
         else: 
             sess = req_session 
 
