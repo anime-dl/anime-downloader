8fdf57abe3949fbe01e300276eddf35a0f7334cc
nate-moo <40650681+nate-moo@users.noreply.github.com>
2020-06-03 07:54:09 -0400

    Update justdubs.py

--- anime_downloader/sites/justdubs.py
+++ anime_downloader/sites/justdubs.py
@@ -48,4 +48,4 @@ class JustDubsEpisode(AnimeEpisode, sitename='justdubs'):
         for a in soup:
             for b in servers:
                 if servers[b] in a.get('src'):
-                    return [(b, a.get('src'))] if servers in a.get('src') else "Unsupported URL"
+                    return [(b, a.get('src'))] if servers in a.get('src') else ""
