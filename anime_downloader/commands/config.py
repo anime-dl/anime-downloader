0c65d935ca58fdece3ec3a214932b9b682b23f90
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-06-08 02:10:01 +0300

    added headers and also made the settings header adaptive to the selescted option

--- anime_downloader/commands/config.py
+++ anime_downloader/commands/config.py
@@ -4,20 +4,21 @@ from anime_downloader.config import Config
 
 data = Config._CONFIG
 
-def create_table(_list):
+def create_table(_list, previous):
     newList = [(x, y) for x, y in zip(range(1,len(_list) + 1), _list)]
-    table = tabulate(newList, tablefmt = "psql")
+    headers = ['SlNo', f'{previous} settings']
+    table = tabulate(newList, headers, tablefmt = "psql")
     table = "\n".join(table.split("\n")[::-1])
     return table
 
-def traverse_json(data):
+def traverse_json(data, previous=''):
     click.clear()
     keys = [*data.keys()]
-    click.echo(create_table(keys))
+    click.echo(create_table(keys, previous))
     val = click.prompt("Select Option", type = int, default = 1) - 1
     
     if type(data[keys[val]]) == dict:
-        traverse_json(data[keys[val]])
+        traverse_json(data[keys[val]], keys[val])
     else:
         click.echo(f"Current value: {data[keys[val]]}")
         newVal = click.prompt(f"Input new value for {keys[val]}", type = str)
@@ -40,7 +41,6 @@ def traverse_json(data):
 def command():
     """
     Presents a list based on the keys in the dictionary.
-
     If the value of said key is not a dictionary, the user will be able to edit the value.
     """
     traverse_json(data)
