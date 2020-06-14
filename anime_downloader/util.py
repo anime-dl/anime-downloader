b17915ef3f9f84b458a35f19895099780fbaa0b5
Vishnunarayan K I <31964688+vn-ki@users.noreply.github.com>
2020-06-14 14:59:19 +0530

    Revert "Make header position configurable" (#408)
    This reverts commit 291143216821f4732b26625659d888d5d9fb3808.

--- anime_downloader/util.py
+++ anime_downloader/util.py
@@ -23,7 +23,6 @@ from urllib.parse import urlparse
 from anime_downloader import session
 from anime_downloader.sites import get_anime_class, helpers
 from anime_downloader.const import desktop_headers
-from anime_downloader.config import Config
 
 logger = logging.getLogger(__name__)
 
@@ -73,21 +72,10 @@ def format_search_results(search_results):
         'Title',
         'Meta',
     ]
-    
-    positions = {"top": 1, "bottom": -1}
-    layout = -1
-    position = Config._CONFIG['dl']['header_position']
-
-    try:
-        layout = positions[position]
-    except KeyError:
-        logger.warn(f"The value '{position}' is not a valid header position")
-
     table = [(i+1, v.title, v.pretty_metadata)
              for i, v in enumerate(search_results)]
     table = tabulate(table, headers, tablefmt='psql')
-
-    table = '\n'.join(table.split('\n')[::layout])
+    table = '\n'.join(table.split('\n')[::-1])
     return table
 
 
