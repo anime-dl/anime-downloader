f765992ea9ee7ce5e08415b3559696294089d18b
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-06-14 11:10:01 +0000

    fixed big issue with gcloud
    It should get way less "File not found (Most likely deleted)" now.
    Error due to very bad id extraction, resulting in incorrect api requests

--- anime_downloader/extractors/gcloud.py
+++ anime_downloader/extractors/gcloud.py
@@ -16,10 +16,14 @@ class Gcloud(BaseExtractor):
         url = url.replace('feurl.com','gcloud.live')
         
         url = url.replace('gcloud.live/v/','gcloud.live/api/source/')
-        if url.find('#') != -1:url = url[:url.find('#')] 
-        url = (url[-url[::-1].find('/'):])
-        data = helpers.post(f'https://gcloud.live/api/source/{url}').json()['data']
 
+        if url.find('#') != -1: #cuts off the # at the end, used for subtitles
+            url = url[:url.find('#')]
+
+        #Returns the last non-empty string from url (separated with /), which is the id
+        source_id = list(filter(lambda x: x, url.split('/')[::-1]))[0]
+        data = helpers.post(f'https://gcloud.live/api/source/{source_id}').json()['data']
+        
         if data == 'Video not found or has been removed':
             logger.warning('File not found (Most likely deleted)')
             return {'stream_url': ''}
