106568e5d0d4852581d22d7e7c9dd7119bb1632c
nate-moo <40650681+nate-moo@users.noreply.github.com>
2020-06-19 17:00:44 -0400

    make pycryptodome optional
    I can't install pycryptodome, and apparently only one provider uses it, so I propose to make it optional

--- setup.py
+++ setup.py
@@ -29,10 +29,11 @@ setup(
         'cfscrape>=2.0.5',
         'requests-cache>=0.4.13',
         'tabulate>=0.8.3',
-        'pycryptodome>=3.8.2',
+
     ],
     extras_require={
         'selescrape': ['selenium'],
+        'pycryptodome>=3.8.2',
         'dev': [
             'pytest',
             'httpretty'
