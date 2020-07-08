f366ede79ee135ffeeb01d160022558e67a2ccfb
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-07-08 22:16:13 +0000

    added edgecase for deleted files

--- anime_downloader/extractors/mp4upload.py
+++ anime_downloader/extractors/mp4upload.py
@@ -10,6 +10,10 @@ logger = logging.getLogger(__name__)
 class MP4Upload(BaseExtractor):
     def _get_data(self):
         soup = str(helpers.get(self.url).text)
+        if 'File was deleted' in soup:
+            logger.warning('File not found (Most likely deleted)')
+            return {'stream_url':''}
+
         regex = r"eval\(function[\W\w]*?</script>"
         script = re.search(regex,soup).group().replace('</script>','')
         script = util.deobfuscate_packed_js(script)
