5407777ee84be3adb54be54816acea8bad3d8cef
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-07-09 15:56:58 +0300

    Update animeonline360.py

--- anime_downloader/extractors/animeonline360.py
+++ anime_downloader/extractors/animeonline360.py
@@ -1,6 +1,6 @@
 from anime_downloader.extractors.base_extractor import BaseExtractor
 from anime_downloader.sites import helpers
-
+import urllib.parse
 class AnimeOnline360(BaseExtractor):
     def _get_data(self):
         try:
@@ -9,7 +9,7 @@ class AnimeOnline360(BaseExtractor):
             anime_type = soup['data-type']
             anime_id = soup['data-post']
             referrer = helpers.get(f"https://animeonline360.me/wp-json/dooplayer/v1/post/{anime_id}?type={anime_type}&source=1").json()['embed_url']
-            stream_url = referrer.split('?source=')[1].split('&')[0].replace('%2F', '/').replace('%3A', ':')
+            stream_url = urllib.parse.unquote(referrer.split('?source=')[1].split('&')[0])
             return {
                 'stream_url': stream_url,
                 'referer': referrer
