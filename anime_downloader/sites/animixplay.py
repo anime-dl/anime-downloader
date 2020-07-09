d21d355735202dafe9b6b63f1af27d10535e71c4
nate-moo <40650681+nate-moo@users.noreply.github.com>
2020-07-09 12:05:13 -0400

    fix some weird git crap

--- anime_downloader/sites/animixplay.py
+++ anime_downloader/sites/animixplay.py
@@ -6,14 +6,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-<<<<<<< HEAD
-
-class AniMixPlay(Anime, sitename='dubbedanime'):
-    sitename='animixplay'
-    @classmethod
-    def search(cls, query):
-        search_results = helpers.soupify(helpers.get('https://animixplay.com/', params = {"q" : query})).select('p.name > a')
-=======
 class AniMixPlay(Anime, sitename='animixplay'):
     sitename='animixplay'
     @classmethod
@@ -21,56 +13,16 @@ class AniMixPlay(Anime, sitename='animixplay'):
         jsonVar = helpers.post("https://animixplay.com/api/search", data = {"qfast": query}, verify = False).json()
         soup = helpers.soupify(jsonVar["result"]).select('div.details a:not([href*=v2]):not([href*=v3]):not([href*=v4])')#
         logger.debug(soup)
->>>>>>> Patch-2
         search_results = [
             SearchResult(
                 title = a.text,
                 url = 'https://animixplay.com' + a.get('href')
             )
-<<<<<<< HEAD
-            for a in search_results
-=======
             for a in soup
->>>>>>> Patch-2
         ]
         return(search_results)
 
     def _scrape_episodes(self):
-<<<<<<< HEAD
-        """
-        _scrape_episodes is function which has to be overridden by the base
-        classes to scrape the episode urls from the web page.
-
-        Parameters
-        ----------
-        soup: `bs4.BeautifulSoup`
-            soup is the html of the anime url after passing through
-            BeautifulSoup.
-
-        Returns
-        -------
-        :code:`list` of :code:`str`
-            A list of episode urls.
-        """
-        return
-
-    def _scrape_metadata(self):
-        """
-        _scrape_metadata is function which has to be overridden by the base
-        classes to scrape the metadata of anime from the web page.
-
-        Parameters
-        ----------
-        soup: :py:class:`bs4.BeautifulSoup`
-            soup is the html of the anime url after passing through
-            BeautifulSoup.
-        """
-        return
-
-class AniMixPlayEpisode(AnimeEpisode, sitename='dubbedanime'):
-    def _get_sources(self):
-        raise NotImplementedError
-=======
         url = self.url
         soup = helpers.soupify(helpers.get(url))
         ep_list = soup.find('div', {'id':'epslistplace'}).get_text()
@@ -88,4 +40,3 @@ class AniMixPlayEpisode(AnimeEpisode, sitename='animixplay'):
     def _get_sources(self):
         logger.debug(self.url)
         return [('vidstream', self.url)]
->>>>>>> Patch-2
