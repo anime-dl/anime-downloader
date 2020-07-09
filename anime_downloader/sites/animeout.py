4ab9b2db89e661efe203787995fbe9f69721cf85
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-07-09 15:43:02 +0000

    code cleanup in animeout

--- anime_downloader/sites/animeout.py
+++ anime_downloader/sites/animeout.py
@@ -4,36 +4,36 @@ import re
 from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
 from anime_downloader.sites import helpers
 
+logger = logging.getLogger(__name__)
+
 class AnimeOut(Anime, sitename='animeout'):
         sitename = 'animeout'
         url = f'https://{sitename}.xyz/'
         @classmethod
         def search(cls, query):
             search_results = helpers.soupify(helpers.get(cls.url, params={'s': query})).select('h3.post-title > a')
-
-            search_results = [
+            return [
                 SearchResult(
-                    title=search_results[a].text,
-                    url=search_results[a].get('href'))
-                for a in range(len(search_results))
+                    title = i.text,
+                    url = i.get('href'))
+                for i in search_results
             ]
-            return(search_results)
+
+
         def _scrape_episodes(self):
             soup = helpers.soupify(helpers.get(self.url))
             elements = soup.select('article.post a')
+            return [i.get('href') for i in elements if 'Direct Download' in i.text]
 
-            episode_links = []
-            for a in elements:
-                if 'Direct Download' in a.text:
-                    episode_links.append(a.get('href'))
-            return [a for a in episode_links]
 
         def _scrape_metadata(self):
             soup = helpers.soupify(helpers.get(self.url))
             self.title = soup.select('h1.page-title')[0].text
 
+
 class AnimeOutEpisode(AnimeEpisode, sitename='animeout'):
         def _get_sources(self):
+            # Should probably be moved to a separate extractor.
             soup = helpers.soupify(helpers.get(self.url))
             link = soup.select('div.Center > p > h2 > a')[0].get('href')
             script = helpers.soupify(helpers.get(link)).select('script')[2]
