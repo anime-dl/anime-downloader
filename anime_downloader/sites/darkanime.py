4b64df88d4e14d899ed89a73c8f7a071ccd0909b
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-07-23 16:54:52 +0000

    fixed sort_sources

--- anime_downloader/sites/darkanime.py
+++ anime_downloader/sites/darkanime.py
@@ -1,8 +1,11 @@
+import logging
 import json
 import re
 
 from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
 from anime_downloader.sites import helpers
+
+logger = logging.getLogger(__name__)
 
 class DarkAnime(Anime, sitename = 'darkanime'):
     sitename = 'darkanime'
@@ -11,25 +14,27 @@ class DarkAnime(Anime, sitename = 'darkanime'):
     def search(cls, query):
         soup = helpers.soupify(helpers.get('https://app.darkanime.stream/api/v1/animes', params={'term': query}).json()['animesHtml'])
         soup = soup.find_all('a', href=True)
-        search_results = [
+        return [
             SearchResult(
                 title = x.find('h3').text.strip(),
                 url = 'https://app.darkanime.stream' + x['href'],
                 )
             for x in soup
             ]
-        return search_results
-    
+
+
     def _scrape_episodes(self):
         html = helpers.soupify(helpers.get(self.url).text)
         eps = html.find('ul', class_='mt-4').find_all('li')
         eps = ['https://app.darkanime.stream' + x.a['href'] for x in eps]
         eps.reverse()
         return eps
-    
+
+
     def _scrape_metadata(self):
-            self.title = helpers.soupify(helpers.get(self.url).text).find_all('h2')[0].text.strip()
-            
+        self.title = helpers.soupify(helpers.get(self.url).text).find_all('h2')[0].text.strip()
+
+
 class DarkAnimeEpisode(AnimeEpisode, sitename='darkanime'):
     def getLink(self, name, _id):
         if name == "trollvid":
@@ -40,21 +45,30 @@ class DarkAnimeEpisode(AnimeEpisode, sitename='darkanime'):
             return "https://www.xstreamcdn.com/v/" + _id
 
     def _get_sources(self):
-        server = self.config.get("server", "mp4upload")
+
+        server_links = {
+            'mp4upload':'https://www.mp4upload.com/embed-{}.html',
+            'trollvid': 'https://trollvid.net/embed/{}',
+        }
+
         resp = helpers.soupify(helpers.get(self.url).text).find_all('script')#[-3].string
-        for a in resp:
-            if type(a.string) != type(None):
-                if 'sources' in a.string:
-                    res = a.string
+        for i in resp:
+            if i.string:
+                if 'sources' in i.string:
+                    res = i.string
+
         hosts = json.loads(re.search(r"(\[[^)]+\])", res).group(1))
-        _type = hosts[0]["type"]
-        try:
-            host = list(filter(lambda video: video["host"] == server and video["type"] == _type, hosts))[0]
-        except IndexError:
-            host = hosts[0]
-            if host["host"] == "mp4upload" and len(hosts) > 1:
-                host = hosts[1]
-        name = host["host"]
-        _id = host["source"]
-        link = self.getLink(name, _id)
-        return [(name, link)]
+        logger.debug('Hosts: {}'.format(hosts))
+
+        sources_list = []
+        for i in hosts:
+            for j in server_links:
+                if i.get('host') in j and i.get('source'):
+                    sources_list.append({
+                        'extractor':j,
+                        'url':server_links[j].format(i['source']),
+                        'server':j,
+                        'version':i['source']
+                        })
+
+        return self.sort_sources(sources_list)
