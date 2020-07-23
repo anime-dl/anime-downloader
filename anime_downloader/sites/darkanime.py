0bdbc67c3422b230339afc16e84047fda3cc76d5
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-07-23 14:08:37 +0300

    removed some whitespaces and changed the regex
    this is not the final commit, i made the pr a draft

--- anime_downloader/sites/darkanime.py
+++ anime_downloader/sites/darkanime.py
@@ -1,4 +1,5 @@
-import json, re
+import json
+import re
 
 from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
 from anime_downloader.sites import helpers
@@ -9,7 +10,6 @@ class DarkAnime(Anime, sitename = 'darkanime'):
 
     @classmethod
     def search(cls, query):
-
         soup = helpers.soupify(helpers.get('https://app.darkanime.stream/api/v1/animes', params={'term': query}).json()['animesHtml'])
         soup = soup.find_all('a', href=True)
         results = [[x.find('h3').text.strip(), 'https://app.darkanime.stream' + x['href']] for x in soup]
@@ -20,25 +20,24 @@ class DarkAnime(Anime, sitename = 'darkanime'):
                 )
             for i in results
             ]
-
         return search_results
-
-
+    
     def _scrape_episodes(self):
         html = helpers.soupify(helpers.get(self.url).text)
         eps = html.find('ul', class_='mt-4').find_all('li')
         eps = ['https://app.darkanime.stream' + x.a['href'] for x in eps]
         eps.reverse()
         return eps
-
+    
     def _scrape_metadata(self):
             self.title = helpers.soupify(helpers.get(self.url).text).find_all('h2')[0].text.strip()
+            
 class DarkAnimeEpisode(AnimeEpisode, sitename='darkanime'):
     def _get_sources(self):
         soup = helpers.soupify(helpers.get(self.url).text)
         players = soup.find_all('iframe')
         sources = soup.find_all('script')[-3].string
-        regex = r"\[([^)]+)\]"
-        sources = json.loads('[' + re.search(regex, sources).group(1) + ']')
+        regex = r"(\[[^)]+\])"
+        sources = json.loads(re.search(regex, sources).group(1))
         link = 'https://www.mp4upload.com/embed-{}.html'.format(sources[-1]['source']) if sources[-1]['host'] == 'mp4upload' else 'https://www.mp4upload.com/embed-{}.html'.format(sources[0]['source'])
         return [('mp4upload', link)]
