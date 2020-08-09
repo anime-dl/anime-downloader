6887477d8e1cae9cff01d621c823fe8527ede41a
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-08-09 20:23:50 +0000

    Update mp4upload.py

--- anime_downloader/extractors/mp4upload.py
+++ anime_downloader/extractors/mp4upload.py
@@ -14,9 +14,10 @@ class MP4Upload(BaseExtractor):
             logger.warning('File not found (Most likely deleted)')
             return {'stream_url':''}
 
-        regex = r"eval\(function[\W\w]*?</script>"
-        script = re.search(regex,soup).group().replace('</script>','')
+        regex = r">\s*(eval\(function[\W\w]*?)</script>"
+        script = re.search(regex,soup).group(1)
         script = util.deobfuscate_packed_js(script)
+
         url = ''
         if re.search(r'player\.src\("([^"]*)',script):
             url = re.search(r'player\.src\("([^"]*)',script).group(1)
