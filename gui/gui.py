00f6f20ef9bb839b26726caa94c30dc7af16bcb3
Osten <balt.758@gmail.com>
2020-08-11 18:42:19 +0000

    Update gui.py

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,9 @@
 import eel
 from anime_downloader.sites import get_anime_class
 from anime_downloader.config import Config
+from anime_downloader import util
+from anime_downloader.sites.anime import AnimeEpisode
+import subprocess
 eel.init('web')
 
 @eel.expose
@@ -21,16 +24,62 @@ def anime_search(query, provider):
 
     eel.populate_search([dict(title=i.title, url=i.url, poster=i.poster) for i in res])
 
-
 @eel.expose
 def get_animeinfo(url):
+    global anime
     print('getting info of: '+ url)
     anime = get_anime_class(url)(url)
     print(anime)
+    print(anime[0].source().stream_url)
     return {
         'title': anime.title,
         'length': len(anime),
     }
+
+def play_mirror_file(player, title, file, ):
+    if player == 'mpv':
+        p = subprocess.Popen([
+            player,
+            '--title={}'.format(title),
+            file,
+            ])
+    else:
+        p = subprocess.Popen([ player, file
+            ])
+    p.wait()
+
+
+    
+@eel.expose
+def download(start, end):
+    episodes = util.parse_ep_str(anime, start + ":" + end)
+    for i in episodes:
+        # Fix util-prase episode range
+        external_downloader = Config['dl']['external_downloader']
+        file_format = Config['dl']['file_format']
+        download_dir = Config['dl']['download_dir']
+        speed_limit= Config['dl']['speed_limit']
+        util.external_download(external_downloader, i, file_format,path=download_dir, speed_limit=speed_limit)
+
+@eel.expose
+def load_vlc(start, end): 
+    #print(anime)
+    #episodes = anime['_episode_urls']
+    #test = AnimeEpisode(episodes[0][1], anime['quality'], anime['_fallback_qualities'])
+    episodes = util.parse_ep_str(anime, start+':'+end)
+    text = "#EXTM3U\n"
+    for i in episodes:
+        print(i.__dict__)
+
+    for i in episodes:
+        text += f"#EXTINF:,Episode {(i.ep_no)}\n" 
+        text += i.source().stream_url + "\n"
+    text_file = open("MirrorList.m3u8", "w")
+    text_file.write(text)
+    text_file.close()
+    print("Playing m3u8 file")
+    play_mirror_file("mpv",anime.title,"MirrorList.m3u8")
+     
 
 
 @eel.expose
