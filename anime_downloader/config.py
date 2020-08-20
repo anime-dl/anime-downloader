4ee908fdca217241784f069bb81720a61387af55
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-08-20 13:49:42 +0000

    Update config.py

--- anime_downloader/config.py
+++ anime_downloader/config.py
@@ -29,7 +29,8 @@ DEFAULT_CONFIG = {
         'file_format':'{animeinfo_anime_title}/{animeinfo_anime_title}_{provider}_{ep_no}',
         'provider':'twist.moe',
         'ratio':50,
-        'fallback_providers':['9anime','vidstream','watchmovie']
+        'fallback_providers':['9anime','vidstream','watchmovie'],
+        'download_metadata':False,
     },
     'watch': {
         'quality': '1080p',
