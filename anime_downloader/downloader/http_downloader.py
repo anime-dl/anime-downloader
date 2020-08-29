eb685562ac92775728a6b185e2f8bdf17639007e
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-08-29 11:25:28 +0000

    Update http_downloader.py

--- anime_downloader/downloader/http_downloader.py
+++ anime_downloader/downloader/http_downloader.py
@@ -7,13 +7,15 @@ import math
 import sys
 
 from anime_downloader.downloader.base_downloader import BaseDownloader
-from anime_downloader import session
 import requests
 import requests_cache
+import multiprocessing as mp
 
-session = session.get_session()
-session = requests
+session = requests.Session()
+session.stream = True
+session.verify = False
 logger = logging.getLogger(__name__)
+
 
 class HTTPDownloader(BaseDownloader):
     def _download(self):
@@ -43,7 +45,6 @@ class HTTPDownloader(BaseDownloader):
         # sys.maxsize/10 is an arbitrary number I judged as safe. (even sys.maxsize/2 works)
 
         # This error doesn't affect downloading the actual file as the writing is done in chunks.
-
         maxsize = int(sys.maxsize/10)
         with open(self.path, "wb") as fp:
             logger.info('Preparing file.')
@@ -57,7 +58,8 @@ class HTTPDownloader(BaseDownloader):
             else:
                 fp.write(b'0' * self._total_size)
 
-        number_of_threads = 8 if self._total_size else 1
+        number_of_threads = 8
+        number_of_threads = number_of_threads if self._total_size > self.chunksize*number_of_threads else 1
         logger.info('Using {} thread{}.'.format(number_of_threads, (number_of_threads > 1) *'s'))
 
         # Creates an empty part file, this comes at the cost of not really knowing if a file is fully completed.
@@ -73,10 +75,15 @@ class HTTPDownloader(BaseDownloader):
         # To get reliable feedback from the threads it uses a dict containing all the info on the threads.
         # This allows maximum download and resumption if any of the threads fail halfway.
 
-        self.thread_report = {}
+        # Init a process manager
+        manager = mp.Manager()
+        q = manager.Queue()
+        pool = mp.Pool(mp.cpu_count() + 2)
+
+        self.thread_report = manager.dict()
         # Prepares the threads with starting info.
         for i in range(number_of_threads):
-            self.thread_report[i] = {}
+            self.thread_report[i] = manager.dict()
             start = int(self.part*i)
 
             # Ensures non-overlapping downloads.
@@ -90,8 +97,13 @@ class HTTPDownloader(BaseDownloader):
             self.thread_report[i]['chunks'] = 0
             self.thread_report[i]['done'] = False
 
+
+        jobs = []
+        # Starts a consumer which writes to the file.
+        consumer = pool.apply_async(self.consumer, (q,))
+
         # Arbitrary max tries, somewhat high number.
-        for attempt in range(number_of_threads*4):
+        for attempt in range(number_of_threads*2):
             for i in range(number_of_threads):
                 # If the thread chunck is done it'll do nothing.
                 # May not be optimal, but better threading would be too complex.
@@ -102,30 +114,18 @@ class HTTPDownloader(BaseDownloader):
                 # Start gets offset based on the previous downloaded chunks.
                 start += (self.thread_report[i].get('chunks',0)*self.chunksize)
                 end = self.thread_report[i]['end']
+                job = pool.apply_async(self.thread_downloader, (url, start, end, headers, i, q,))
+                jobs.append(job)
 
-                t = threading.Thread(target=self.thread_downloader,
-                    kwargs={'url': url, 'start':start, 'end': end, 'headers':headers, 'number':i})
-                t.setDaemon(True)
-                t.start()
+            for job in jobs:
+                job.get()
 
-            main_thread = threading.current_thread()
-            for t in threading.enumerate():
-                if t is main_thread:
-                    continue
-                t.join()
 
-        """
-        # Collects all the files and places them in one.
-        # Doesn't work correctly!!!
-        with open(self.path, "wb") as file:
-            for i in range(number_of_threads):
-                with open(self.path+"_"+str(i), "rb") as part:
-                    part.seek(int(self.part*i))
-                    #var = part.tell()
-                    data = part.read()
-                    file.write(data)
-                #os.remove(self.path+"_"+str(i))
-        """
+        # Kill the consumer
+        q.put('kill')
+        pool.close()
+        pool.join()
+
 
     def _non_range_download(self):
         url = self.source.stream_url
@@ -134,7 +134,7 @@ class HTTPDownloader(BaseDownloader):
         }
         if self.source.referer:
             headers['Referer'] = self.source.referer
-        r = session.get(url, headers=headers, stream=True)
+        r = session.get(url, headers=headers)
 
         if r.status_code == 200:
             with open(self.path, 'wb') as f:
@@ -144,28 +144,47 @@ class HTTPDownloader(BaseDownloader):
                         self.report_chunk_downloaded()
 
 
-    def thread_downloader(self, url, start, end, headers, number):
+    def thread_downloader(self, url, start, end, headers, number, q):
         if end:
             headers['Range'] = 'bytes=%d-%d' % (start, end)
 
         # specify the starting and ending of the file
         # request the specified part and get into variable
-        with requests.get(url, headers=headers, stream=True, verify=False) as r:
+        with session.get(url, headers=headers) as r:
             if not (r.headers.get('content-length') or
                     r.headers.get('Content-length') or
                     r.headers.get('Content-Length') or
                     r.headers.get('Transfer-Encoding') == 'chunked') or r.status_code not in [200, 206]:
                 return False
 
-            with open(self.path, "r+b") as fp:
-                fp.seek(start)
-                for chunk in r.iter_content(chunk_size=self.chunksize):
-                    if chunk:
-                        fp.write(chunk)
-                        self.thread_report[number]['chunks'] += 1
-                        self.report_chunk_downloaded()
+            for chunk in r.iter_content(chunk_size=self.chunksize):
+                if chunk:
+                    q.put((start, chunk, number))
 
             self.thread_report[number]['done'] = True
+
+
+    def consumer(self, q):
+        """Listen to consumer queue and write file using offset
+        :param q: Consumer queue
+        :param name: file name
+        """
+        f = open(self.path, 'w+b')
+        
+        while 1:
+            m = q.get()
+            if m == 'kill':
+                break
+
+            offset, chunk, number = m
+            f.seek(offset+(self.thread_report[number]['chunks']*self.chunksize))
+            f.write(chunk)
+            self.thread_report[number]['chunks'] += 1
+            self.report_chunk_downloaded()
+            f.flush()
+
+        f.close()
+        return self.thread_report
 
 
 def set_range(start=0, end='', headers=None):
