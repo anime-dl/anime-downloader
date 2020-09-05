df0036528ed3e3b144dc86fc38ef14ceb1f4ae3d
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-09-05 16:13:44 +0300

    Update __init__.py

--- anime_downloader/downloader/__init__.py
+++ anime_downloader/downloader/__init__.py
@@ -1,6 +1,6 @@
 from anime_downloader.downloader.http_downloader import HTTPDownloader
 from anime_downloader.downloader.external_downloader import ExternalDownloader
-from anime_downloader.downloader.SmartDL import SmartDL
+from anime_downloader.downloader.SmartDL import pySmartDL
 
 def get_downloader(downloader):
     """get_downloader returns the proper downloader class
