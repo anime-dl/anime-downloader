dee1bc2b92f4b05ef01acce7d6ca9e0337f126bb
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2020-09-15 23:55:08 +0300

    Add files via upload

---
+++ anime_downloader/sites/tenshimoe.py
@@ -0,0 +1,37 @@
+import json
+import os
+from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
+from anime_downloader.sites import helpers
+
+class TenshiMoe(Anime, sitename = 'tenshi.moe'):
+
+    sitename = 'tenshi.moe'
+
+    @classmethod
+    def search(cls, query):
+        soup = helpers.soupify(helpers.get('https://tenshi.moe/anime', params={'q': query}).text)
+        soup = soup.find('ul', class_="loop anime-loop list")
+        results = soup.select('li')
+        results = [[x.a['title'], x.a['href']] for x in results]
+        return [
+            SearchResult(
+                title=x[0],
+                url=x[1],
+                )
+            for x in results
+            ]
+
+    def _scrape_episodes(self):
+        soup = helpers.soupify(helpers.get(self.url).text).find_all('ul', class_="loop episode-loop list")
+        eps = [x.a['href'] for x in soup]
+        return eps
+
+    def _scrape_metadata(self):
+            soup = helpers.soupify(helpers.get(self.url).text)
+            self.title = soup.title.text.split('—')[0].strip()
+            
+class TenshiMoeEpisode(AnimeEpisode, sitename='tenshi.moe'):
+    def _get_sources(self):
+        soup = helpers.soupify(helpers.get(self.url).text)
+        link = soup.find_all('source', type="video/mp4")[-1]['src']
+        return [('no_extractor', link)]
