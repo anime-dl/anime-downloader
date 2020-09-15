020940635a07e0a90fa00cd70ae409c1b8e9cd2c
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-09-15 15:33:17 +0200

    small fixes

--- anime_downloader/sites/animebinge.py
+++ anime_downloader/sites/animebinge.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import re
 from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
 from anime_downloader.sites import helpers
 from anime_downloader.extractors import get_extractor
@@ -12,7 +13,7 @@ class AnimeBinge(Anime, sitename='animebinge'):
 
     @classmethod
     def search(cls, query):
-        response = helpers.get('https://animebinge.net/search', params={'term': query}).text
+        response = helpers.get('https://animebinge.net/search', params={'term': query})
         soup = helpers.soupify(response)
         results = soup.select('a#epilink')
 
@@ -20,46 +21,55 @@ class AnimeBinge(Anime, sitename='animebinge'):
             SearchResult(
                 title=x.text,
                 url=x['href']
-                )
+            )
             for x in results
-            ]
+        ]
         return search_results
 
     def _scrape_episodes(self):
-        soup = helpers.soupify(helpers.get(self.url).text).select_one('div.anime#sub')
-        eps = soup.select('div.episode-wrap')
-        eps = [x.a['href'] for x in eps]
+        eps = helpers.soupify(helpers.get(self.url)).select('div.episode-wrap > a')
+        eps = [x['href'] for x in eps]
         eps.reverse()
         return eps
 
     def _scrape_metadata(self):
-        self.title = helpers.soupify(helpers.get(self.url).text).select_one('div.contingo > p').text
+        self.title = helpers.soupify(helpers.get(self.url)).select_one('div.contingo > p').text
+
 
 class AnimeBingeEpisode(AnimeEpisode, sitename='animebinge'):
     def _get_sources(self):
-        soup = helpers.soupify(helpers.get(self.url).text)
-        sources = soup.find_all('script')
+        html = helpers.get(self.url).text
+        # Matches:
+        # var episode = {"id":"187961",
+        #                "url":"https:\/\/animebinge.net\/episode\/187961-yakusoku-no-neverland-episode-1",
+        #                "lang":"dubbed"}; </script>
+        # And parses the json in the script.
+        episode_regex = r'var\s*episode\s*=\s*({[\W\w]*?);\s*<\/script>'
 
-        for source in sources:
-            if 'var episode' in str(source):
-                sources = json.loads(str(source).split('var episode = ')[1].replace(';\n', '').replace('</script>', '').strip())['videos']
-                break
+        source = re.search(episode_regex, str(html))
+        if source:
+            print(source.group(1))
+            source_json = json.loads(source.group(1))['videos']
+        else:
+            return ''
 
-        logger.debug('Sources: {}'.format(sources))
+        logger.debug('Sources: {}'.format(source_json))
 
         mappings = {
             'mp4upload': 'https://www.mp4upload.com/embed-{}.html',
-            'trollvid': 'https://trollvid.net/embed/{}'
+            'trollvid': 'https://trollvid.net/embed/{}',
+            'xstreamcdn': 'https://xstreamcdn.com/v/{}'
         }
 
         sources_list = []
-        for i in sources:        
-            extractor = 'no_extractor' if not get_extractor(i['host']) else i['host']
-            sources_list.append({
-            'extractor':extractor,
-            'url': mappings.get(i['host'], None).format(i['id']),
-            'server':i['host'],
-            'version':i.get('type','subbed')
-            })
+        for i in source_json:
+            if mappings.get(i.get('host')):
+                extractor = 'no_extractor' if not get_extractor(i['host']) else i['host']
+                sources_list.append({
+                    'extractor': extractor,
+                    'url': mappings[i['host']].format(i['id']),
+                    'server': i['host'],
+                    'version': i.get('type', 'subbed')
+                })
 
         return self.sort_sources(sources_list)
