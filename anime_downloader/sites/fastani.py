8d47beed8bbf2802ecd6075c3ac5ace34e24f749
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-09-16 15:41:57 +0200

    PEP fixes

--- anime_downloader/sites/fastani.py
+++ anime_downloader/sites/fastani.py
@@ -1,10 +1,10 @@
-
 from anime_downloader.sites.anime import Anime, AnimeEpisode, SearchResult
 from anime_downloader.sites import helpers
 import re
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 class FastAni(Anime, sitename="fastani"):
 
@@ -14,11 +14,11 @@ class FastAni(Anime, sitename="fastani"):
     def getToken(cls):
         site_text = helpers.get("https://fastani.net").text
 
-        #Path to js file, e.g /static/js/main.f450dd1c.chunk.js - which contains the token
-        js_location = "https://fastani.net" + re.search("src=\"(\/static\/js\/main.*?)\"", site_text).group(1)
+        # Path to js file, e.g /static/js/main.f450dd1c.chunk.js - which contains the token
+        js_location = "https://fastani.net" + re.search(r"src=\"(\/static\/js\/main.*?)\"", site_text).group(1)
         js = helpers.get(js_location).text
 
-        #Get authorization token, e.g: {authorization:"Bearer h8X2exbErErNSxRnr6sSXAE2ycUSyrbU"}
+        # Get authorization token, e.g: {authorization:"Bearer h8X2exbErErNSxRnr6sSXAE2ycUSyrbU"}
         token = re.search("authorization:.*?\"(.*?)\"", js).group(1)
 
         return {"authorization": token}
@@ -26,13 +26,13 @@ class FastAni(Anime, sitename="fastani"):
     @classmethod
     def search(cls, query):
         headers = cls.getToken()
-        results = helpers.get(f"https://fastani.net/api/data?page=1&search={query}&tags=&years=", headers = headers).json()
+        results = helpers.get(f"https://fastani.net/api/data?page=1&search={query}&tags=&years=", headers=headers).json()
 
         return [
             SearchResult(
-                title = x.get('title').get("english"),
-                #Need to know selected anime and original query for _scrape_episodes
-                url = f"https://fastani.net/{selected}/{query}"
+                title=x.get('title').get("english"),
+                # Need to know selected anime and original query for _scrape_episodes
+                url=f"https://fastani.net/{selected}/{query}"
             )
             for selected, x in zip(range(len(results["animeData"]["cards"])), results["animeData"]["cards"])
         ]
@@ -42,22 +42,22 @@ class FastAni(Anime, sitename="fastani"):
         headers = cls.getToken()
         split = self.url.split("/")
         query, selected = split[-1], int(split[-2])
-        anime = helpers.get(f"https://fastani.net/api/data?page=1&search={query}&tags=&years=", headers = headers).json()
+        anime = helpers.get(f"https://fastani.net/api/data?page=1&search={query}&tags=&years=", headers=headers).json()
 
         cdnData = anime["animeData"]["cards"][selected]["cdnData"]
 
-        #Get all episodes from all seasons of the anime
-        #JSON Example:
+        # Get all episodes from all seasons of the anime
+        # JSON Example:
         """
         {
-	'seasons': [{
+        'seasons': [{
           'episodes': [{
             'file': 'https://private.fastani.net/Naruto/Season 1/Naruto S01E001.mp4',
-	    'directory': 'https://private.fastani.net/Naruto/Season 1',
-	    'timestamp': '2020-09-11T16:22:48.744Z',
-	    'thumb': 'https://private.fastani.net/Naruto/Season 1/thumbs/20_thumbnail_001.jpg',
-	    'title': 'Enter: Naruto Uzumaki!'
-	    }
+        'directory': 'https://private.fastani.net/Naruto/Season 1',
+        'timestamp': '2020-09-11T16:22:48.744Z',
+        'thumb': 'https://private.fastani.net/Naruto/Season 1/thumbs/20_thumbnail_001.jpg',
+        'title': 'Enter: Naruto Uzumaki!'
+        }
             ...
           ]
         }
@@ -71,8 +71,9 @@ class FastAni(Anime, sitename="fastani"):
         headers = cls.getToken()
         split = self.url.split("/")
         query, selected = split[-1], int(split[-2])
-        anime = helpers.get(f"https://fastani.net/api/data?page=1&search={query}&tags=&years=", headers = headers).json()
+        anime = helpers.get(f"https://fastani.net/api/data?page=1&search={query}&tags=&years=", headers=headers).json()
         self.title = anime["animeData"]["cards"][selected]["title"]["english"]
+
 
 class FastAniEpisode(AnimeEpisode, sitename='fastani'):
     def _get_sources(self):
