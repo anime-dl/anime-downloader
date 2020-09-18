d6d0b61ec958eaa04005cb263ed90d7e0ac96932
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-09-18 14:03:03 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'requests-cache>=0.4.13',
         'tabulate>=0.8.3',
         'pycryptodome>=3.8.2',
+        'm3u8-dl>=0.2.1'
     ],
     extras_require={
         'selescrape': ['selenium'],
