4513bae40b4580d5c71f1c99c8f8ce3a1cb52d94
Iggy <44432163+IguanasInPyjamas@users.noreply.github.com>
2020-09-26 07:11:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'cfscrape>=2.0.5',
         'requests-cache>=0.4.13',
         'tabulate>=0.8.3',
+        'PyQt5>=5.15.1',
         'pycryptodome>=3.8.2',
     ],
     extras_require={
