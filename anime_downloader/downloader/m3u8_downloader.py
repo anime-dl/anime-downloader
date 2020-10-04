78b3806846fcb0eb355af04e71f62343cc2dbd93
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-10-04 13:43:52 +0200

    Update m3u8_downloader.py

--- anime_downloader/downloader/m3u8_downloader.py
+++ anime_downloader/downloader/m3u8_downloader.py
@@ -1,21 +1,9 @@
 from anime_downloader.downloader.base_downloader import BaseDownloader
-from anime_downloader.util import get_m3u8_command
-import time
-import sys
+from anime_downloader.util import download_m3u8
 import os
-import subprocess
 
 
 class m3u8(BaseDownloader):
     def _download(self):
         expected_file = os.path.abspath(self.path)
-        cmd = f'm3u8-dl -r {self.source.referer} --insecure -t 16 {self.source.stream_url} {expected_file}'
-        cmd = get_m3u8_command(cmd, expected_file)
-        if cmd:
-            p = subprocess.Popen(cmd.split(' '))
-            return_code = p.wait()
-
-            if return_code != 0:
-                # Sleep for a while to make sure downloader exits correctly
-                time.sleep(2)
-                sys.exit(1)
+        download_m3u8(self.source.stream_url, self.source.referer, expected_file)
