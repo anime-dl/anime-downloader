526fde413a7cc8860f6d62d2d811065eae4a0847
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-10-07 20:35:13 +0200

    Update eplay.py

--- anime_downloader/extractors/eplay.py
+++ anime_downloader/extractors/eplay.py
@@ -1,11 +1,10 @@
-
 from anime_downloader.extractors.base_extractor import BaseExtractor
 from anime_downloader.sites import helpers
 
 
 class EPlay(BaseExtractor):
     def _get_data(self):
-        direct_link=helpers.soupify(helpers.get(self.url)).source.get("src")
+        direct_link = helpers.soupify(helpers.get(self.url)).source.get("src")
 
         return {
             'stream_url': direct_link,
