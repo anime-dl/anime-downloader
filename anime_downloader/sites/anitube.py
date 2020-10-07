a0f3dc3c20bfd640a257c2bd22d59c85d6ebc0bf
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-10-07 14:54:33 +0200

    Update anitube.py

--- anime_downloader/sites/anitube.py
+++ anime_downloader/sites/anitube.py
@@ -13,23 +13,23 @@ class AniTube(Anime, sitename="anitube"):
         results = html.select('div.aniItem > a')
 
         return [
-                SearchResult(
-                    title=x['title'].split(
-                        'Temporada')[0].split('– Todos')[0].split(
-                        'Todos os')[0],
-                    url=x['href']
-                )
-                for x in html.select('div.aniItem > a')
-            ]
+            SearchResult(
+                title=x['title'].split(
+                    'Temporada')[0].split('– Todos')[0].split(
+                    'Todos os')[0],
+                url=x['href']
+            )
+            for x in html.select('div.aniItem > a')
+        ]
         return results
 
     def _scrape_episodes(self):
-        html = helpers.soupify(helpers.get(self.url).text)
+        html = helpers.soupify(helpers.get(self.url))
         eps = html.select('div.pagAniListaContainer.targetClose > a')
         return [x['href'] for x in eps]
 
     def _scrape_metadata(self):
-        html = helpers.soupify(helpers.get(self.url).text)
+        html = helpers.soupify(helpers.get(self.url))
         title = html.select_one('div.mwidth > h1').text
         title = title.split('– Todos')[0]
         title = title.split('Todos os')[0]
@@ -38,12 +38,12 @@ class AniTube(Anime, sitename="anitube"):
 
 class AniTubeEpisode(AnimeEpisode, sitename='anitube'):
     def _get_sources(self):
-        html = helpers.soupify(helpers.get(self.url).text)
+        html = helpers.soupify(helpers.get(self.url))
         scripts = html.find_all('script', type="text/javascript")
 
         for script in scripts:
             if 'var play' in str(script):
                 js = script
 
-        link = re.search("file.*?[\"'](.*?)['\"]", str(js)).group(1)
+        link = re.search(r"file.*?[\"'](.*?)['\"]", str(js)).group(1)
         return [('no_extractor', link)]
