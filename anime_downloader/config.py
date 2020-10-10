848979c7edc50749f96c729415a9a6573fa8d7eb
Blatzar <46196380+Blatzar@users.noreply.github.com>
2020-10-10 14:02:31 +0200

    Update config.py

--- anime_downloader/config.py
+++ anime_downloader/config.py
@@ -20,7 +20,7 @@ DEFAULT_CONFIG = {
         'provider': 'twist.moe',
         'external_downloader': '',
         'aria2c_for_torrents': False,
-        'aria2c_log_level': 'notice',
+        'aria2c_log_level': 'error',
         'selescrape_browser': None,
         'selescrape_browser_executable_path': None,
         'selescrape_driver_binary_path': None,
