53ea7955b9e4b81095468510df8cf8201c5e88c8
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2020-10-12 15:29:18 +0100

    autopep8

--- anime_downloader/sites/erairaws.py
+++ anime_downloader/sites/erairaws.py
@@ -22,64 +22,70 @@ class EraiRaws(Anime, sitename='erai-raws'):
     @classmethod
     def bypass(self):
         host = "https://erai-raws.info"
-        resp = helpers.get("https://check.ddos-guard.net/check.js", cache=False).text
+        resp = helpers.get(
+            "https://check.ddos-guard.net/check.js", cache=False).text
 
         # new Image().src = '/.well-known/ddos-guard/id/WaEVEyURh4MduAdI'; -> /.well-known/ddos-guard/id/WaEVEyURh4MduAdI
         ddosBypassPath = re.search("'(.*?)'", resp).groups()[0]
         return helpers.get(host + ddosBypassPath, cache=False).cookies
 
     def parse(self, server):
-        cookies=self.bypass()
-        soup=helpers.soupify(helpers.get(server, cookies=cookies))
+        cookies = self.bypass()
+        soup = helpers.soupify(helpers.get(server, cookies=cookies))
         # A mix of episodes and folders containing episode
         # Keeping the nodes to check the titles later for quality selection
-        linkNodes=soup.select("td[title] > a[href]")
-        folderIndices=[i for i, x in enumerate(linkNodes) if "folder" in x.get("href")]
+        linkNodes = soup.select("td[title] > a[href]")
+        folderIndices = [i for i, x in enumerate(
+            linkNodes) if "folder" in x.get("href")]
 
         while len(folderIndices) > 0:
             for index in folderIndices:
-                link=linkNodes[index].get("href")
+                link = linkNodes[index].get("href")
 
                 # Sometimes we get a 403 and have to wait for 5 seconds
                 for i in range(6):
                     try:
-                        soup=helpers.soupify(helpers.get(link, cookies=cookies))
+                        soup = helpers.soupify(
+                            helpers.get(link, cookies=cookies))
                         break
                     except HTTPError:
                         time.sleep(5)
-                        cookies=self.bypass()
-                        soup=helpers.soupify(helpers.get(link, cookies=cookies))
-                
+                        cookies = self.bypass()
+                        soup = helpers.soupify(
+                            helpers.get(link, cookies=cookies))
+
                 # Replace the folder node with all the nodes of what the folder contains
-                linkNodes[index]=soup.select("td[title] > a[href]")
+                linkNodes[index] = soup.select("td[title] > a[href]")
 
             # Flatten list, e.g. [node, node, [node, node], node] -> [node, node, node, node, node]
-            linkNodes=[i for x in linkNodes for i in x]
+            linkNodes = [i for x in linkNodes for i in x]
 
             # Maybe due to the flattening, but sometimes <a class="responsiveInfoTable" href="https://srv9.erai-ddl3.info/5757d93aae57a6916eed08bc368ad8b7" target="_blank">[Erai-raws] One Piece - 915 [1080p][Multiple Subtitle].mkv</a> becomes [Erai-raws] One Piece - 915 [1080p][Multiple Subtitle].mkv which leads to an error when getting the links
             for x, y in enumerate(linkNodes):
                 if type(y) == NavigableString:
                     linkNodes[x] = y.parent
 
-            folderIndices=[i for i, x in enumerate(linkNodes) if "folder" in x.get("href")]
+            folderIndices = [i for i, x in enumerate(
+                linkNodes) if "folder" in x.get("href")]
 
-        links=[x.get("href") for x in linkNodes if self.quality in x.text]
+        links = [x.get("href") for x in linkNodes if self.quality in x.text]
 
         return links
 
     def getTorrents(self, soup, cookies):
         # Clickable nodes, such as: Notifications, Episodes, Batch, etc
         # We are only interested in Episode/Batch
-        nodes=soup.select("a.aa_ss")
-        episode_nodes=[x for x in nodes if x.text == "Episodes"]
+        nodes = soup.select("a.aa_ss")
+        episode_nodes = [x for x in nodes if x.text == "Episodes"]
         load = "load_more_0"
 
         if not episode_nodes:
             logger.warn("Episodic torrents not found, using batch torrents...")
-            batch_torrents=[x for x in nodes if x.text == "Batch"]
+            batch_torrents = [x for x in nodes if x.text == "Batch"]
 
             if not batch_torrents:
-                logger.error("Neither episode torrents nor batch torrents were found")
+                logger.error(
+                    "Neither episode torrents nor batch torrents were found")
                 return
 
             load = "load_more_3"
@@ -97,13 +103,15 @@ class EraiRaws(Anime, sitename='erai-raws'):
             'order': 'DESC'
         }
         """
-        post_data["query"] = json.dumps(json.loads(re.search("posts.*?(\{.*?order.*?\})", str(soup)).group(1).replace("\\", "")), separators=(",", ":"))
+        post_data["query"] = json.dumps(json.loads(re.search(
+            "posts.*?(\{.*?order.*?\})", str(soup)).group(1).replace("\\", "")), separators=(",", ":"))
 
         episodes = []
 
         for page in range(max_page + 1):
             post_data["page"] = page
-            resp = helpers.post("https://erai-raws.info/wp-admin/admin-ajax.php", data=post_data, cookies=cookies)
+            resp = helpers.post(
+                "https://erai-raws.info/wp-admin/admin-ajax.php", data=post_data, cookies=cookies)
 
             if resp:
                 soup = helpers.soupify(resp)
@@ -111,15 +119,18 @@ class EraiRaws(Anime, sitename='erai-raws'):
                 # List of tuples of (quality, magnet)
                 # episodes.extend(
                 # [x[1] for x in
-                eps = [(x[0].text, x[1]["href"]) for y in [list(zip(x.select("i.sp_p_q"), x.select("a.load_more_links[href*=magnet]"))) for x in soup.select("article div:has(i.sp_p_q):has(a.load_more_links[href*=magnet])")] for x in y] 
+                eps = [(x[0].text, x[1]["href"]) for y in [list(zip(x.select("i.sp_p_q"), x.select("a.load_more_links[href*=magnet]")))
+                                                           for x in soup.select("article div:has(i.sp_p_q):has(a.load_more_links[href*=magnet])")] for x in y]
                 # if self.quality in x[0]]
 
                 # Filter by quality
                 filtered_eps = [x[1] for x in eps if self.quality in x[0]]
 
                 if not filtered_eps:
-                    logger.warning(f"Quality {self.quality} not found. Trying {self.QUALITIES[not self.QUALITIES.index(self.quality)]}")
-                    filtered_eps = [x[1] for x in eps if self.QUALITIES[not self.QUALITIES.index(self.quality)]]
+                    logger.warning(
+                        f"Quality {self.quality} not found. Trying {self.QUALITIES[not self.QUALITIES.index(self.quality)]}")
+                    filtered_eps = [
+                        x[1] for x in eps if self.QUALITIES[not self.QUALITIES.index(self.quality)]]
 
                 for ep in filtered_eps:
                     # Sometimes duplication happens
@@ -130,9 +141,11 @@ class EraiRaws(Anime, sitename='erai-raws'):
 
     @classmethod
     def search(cls, query):
-        cookies=cls.bypass()
-        soup = helpers.soupify(helpers.get("https://erai-raws.info/anime-list/", cookies=cookies))
-        result_data = soup.find("div", {"class": "shows-wrapper"}).find_all("a")
+        cookies = cls.bypass()
+        soup = helpers.soupify(helpers.get(
+            "https://erai-raws.info/anime-list/", cookies=cookies))
+        result_data = soup.find(
+            "div", {"class": "shows-wrapper"}).find_all("a")
         titles = [x.text.strip() for x in result_data]
 
         # Erai-raws doesnt have a search that I could find - so I've opted to implement it myself
@@ -152,15 +165,15 @@ class EraiRaws(Anime, sitename='erai-raws'):
         if self.quality not in self.QUALITIES:
             self.quality = "720p"
 
-        cookies=self.bypass()
+        cookies = self.bypass()
         soup = helpers.soupify(helpers.get(self.url, cookies=cookies))
 
-        #Check if anime has DDL - as of writing this, most do not
-        ddl=soup.select("div.ddmega > a[href]")[0]
+        # Check if anime has DDL - as of writing this, most do not
+        ddl = soup.select("div.ddmega > a[href]")[0]
 
         # As opposed to Subs
         if ddl.text == "DDL":
-            server=ddl.get("href")
+            server = ddl.get("href")
             return self.parse(server)
         else:
             # use torrent
@@ -178,7 +191,7 @@ class EraiRawsEpisode(AnimeEpisode, sitename='erai-raws'):
             return [("no_extractor", self.url)]
 
         # Headers have to be really good
-        headers={
+        headers = {
             'cache-control': 'max-age=0',
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/56.0',
@@ -193,26 +206,29 @@ class EraiRawsEpisode(AnimeEpisode, sitename='erai-raws'):
 
         for i in range(4):
             # Using a request session as helpers is lacking the head function, and having a session makes everything more seamless
-            session=requests.session()
-            resp=session.get(self.url, cookies=EraiRaws.bypass(), headers=headers)
-            page=resp.text
+            session = requests.session()
+            resp = session.get(
+                self.url, cookies=EraiRaws.bypass(), headers=headers)
+            page = resp.text
 
             """
             Example:
             --------
             $('.download-timer').html("<a class='btn btn-free' href='https://srv9.erai-ddl3.info/486dbafc9628c685c5e67c14d438a425?pt=UmpjMllXWlNSbVl4Vm5CcVNqRnBSVlVyUm5WcVVUMDlPdlF5TEtZVi9TZ2JXc01DOGc2WkhIYz0%3D'>download now</a>");
             """
-            download_link=re.search("\.download-timer.*?html.*?href=['\"](.*?)['\"]", page).group(1)
+            download_link = re.search(
+                "\.download-timer.*?html.*?href=['\"](.*?)['\"]", page).group(1)
 
             # Required - if you don't wait, you generally won't get the actual download link
             time.sleep(10)
 
-            resp=requests.head(download_link, headers=headers, cookies=resp.cookies)
+            resp = requests.head(
+                download_link, headers=headers, cookies=resp.cookies)
 
             if resp.status_code == 302:
-                download_link=resp.headers.get("location")
+                download_link = resp.headers.get("location")
                 break
 
-            self.url=download_link
+            self.url = download_link
 
         return [("no_extractor", download_link)]
