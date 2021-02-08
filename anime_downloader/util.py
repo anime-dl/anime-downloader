e2a9742d405a00ad10219528e1540455f0411d87
nate-moo <40650681+nate-moo@users.noreply.github.com>
2021-02-08 16:11:27 -0500

    logic to detect single episode shows/movies

--- anime_downloader/util.py
+++ anime_downloader/util.py
@@ -221,25 +221,23 @@ def print_episodeurl(episode):
         "magnet") else episode.source().stream_url
     print(unquote(url))
 
-
 def play_episode(episode, *, player, title, episodes="0:0"):
     if player == 'mpv':
-        p = subprocess.Popen([player, 
-            f'--title={title}', 
-            f'--referrer={episode.source().referer}', 
-            f'--user-agent={get_random_header()["user-agent"]}', 
+        p = subprocess.Popen([player,
+            f'--title={title}',
+            f'--referrer={episode.source().referer}',
+            f'--user-agent={get_random_header()["user-agent"]}',
             episode.source().stream_url])
     elif player == "android":
+        p = subprocess.Popen(['am', 'start', '-a', 'android.intent.action.VIEW', '-t', 'video/*', '-d', f'{episode.source().stream_url}'])
         if episodes == None or ':' in episodes:
-            p = subprocess.Popen(['am', 'start', '-a', 'android.intent.action.VIEW', '-t', 'video/*', '-d', f'{episode.source().stream_url}'])
-            input("Press enter to continue\n")
-        else:
-            p = subprocess.Popen(['am', 'start', '-a', 'android.intent.action.VIEW', '-t', 'video/*', '-d', f'{episode.source().stream_url}'])
+            if "0:1" in episodes:
+                pass
+            else:
+                input("Press enter to continue\n")
     else:
-        p = subprocess.Popen([player, episode.source().stream_url
-                              ])
+        p = subprocess.Popen([player, episode.source().stream_url])
     p.wait()
-
 
 def print_info(version):
     logger.info('anime-downloader {}'.format(version))
