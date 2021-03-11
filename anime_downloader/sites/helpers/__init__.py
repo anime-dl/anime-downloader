b217a8d3868c6bbda0c791a0aad87a86c0723893
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2021-03-11 15:34:39 +0200

    Update __init__.py

--- anime_downloader/sites/helpers/__init__.py
+++ anime_downloader/sites/helpers/__init__.py
@@ -1,3 +1,3 @@
 from anime_downloader.sites.helpers.request import *
 from anime_downloader.sites.helpers.util import not_working
-from anime_downloader.sites.helpers.unpacker import detect, unpack
+from anime_downloader.sites.helpers.unpacker import detect_packed_js, unpack_packed_js
