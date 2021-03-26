3f649db0cb451de62d96c2c05ccefbb7346cc883
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2021-03-26 21:33:33 +0200

    Update unpacker.py

--- anime_downloader/sites/helpers/unpacker.py
+++ anime_downloader/sites/helpers/unpacker.py
@@ -1,10 +1,11 @@
-from jsbeautifier.unpackers import javascriptobfuscator, myobfuscate, packer
-
-UNPACKERS = [javascriptobfuscator, myobfuscate, packer]
-
-
-def deobfuscate_packed_js(js):
-    for unpacker in UNPACKERS:
-        if unpacker.detect(js):
-            return unpacker.unpack(js)
-    return js
+try:
+    from jsbeautifier.unpackers import javascriptobfuscator, myobfuscate, packer
+    UNPACKERS = [javascriptobfuscator, myobfuscate, packer]
+    def deobfuscate_packed_js(js):
+        for unpacker in UNPACKERS:
+            if unpacker.detect(js):
+                return unpacker.unpack(js)
+        return js
+except ImportError:
+    def deobfuscate_packed_js(js):
+        return js
