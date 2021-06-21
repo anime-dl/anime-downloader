52bda62aed90138fc8adf3eb9e969667d2af5528
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2021-06-21 20:43:29 +0300

    added file not found error

--- anime_downloader/extractors/yourupload.py
+++ anime_downloader/extractors/yourupload.py
@@ -3,6 +3,7 @@ import re
 
 from anime_downloader.extractors.base_extractor import BaseExtractor
 from anime_downloader.sites import helpers
+from requests.exceptions import HTTPError
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +11,13 @@ logger = logging.getLogger(__name__)
 class Yourupload(BaseExtractor):
     def _get_data(self):
         regex = r"file: '([^']*)"
-        file = re.search(regex, helpers.get(self.url).text).group(1)
+        try:
+            response = helpers.get(self.url)
+        except HTTPError:
+            logger.error('File not found.')
+            raise
+
+        file = re.search(regex, response.text).group(1)
         return {
             'stream_url': file,
             'referer': self.url
